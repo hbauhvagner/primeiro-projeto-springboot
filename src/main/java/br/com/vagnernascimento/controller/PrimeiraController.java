e92dd9d5175cb90e8cd34b6dd7a8cd31ac5000d7
hbauhvagner <hbauhvagner@gmail.com>
2023-10-29 01:13:04 -0300

    Path Params

--- src/main/java/br/com/vagnernascimento/controller/PrimeiraController.java
+++ src/main/java/br/com/vagnernascimento/controller/PrimeiraController.java
@@ -1,15 +1,29 @@
 package br.com.vagnernascimento.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/primeiraController")
 public class PrimeiraController {
     
-    @GetMapping("/primeiroMetodo")
-    public String primeiroMetodo() {
-        return "Meu primeiro metodo api rest";
+    @GetMapping("/primeiroMetodo/{id}")
+    public String primeiroMetodo(@PathVariable String id) {
+        return "O parametro é " + id;
+    }
+
+    @GetMapping("/metodoComQueryParams")
+    public String metodoComQueryParams(@RequestParam String id) {
+        return "O parametro com metodoComQueryParams é" + id;
+    }
+
+    @GetMapping("/metodoComQueryParams2")
+    public String metodoComQueryParams2(@RequestParam Map<String, String> allParams) {
+        return "O parametro com metodoComQueryParams2 é " + allParams.entrySet();
     }
 }
