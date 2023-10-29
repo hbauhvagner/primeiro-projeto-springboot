9e3790198beccb7a049ba4a81e8b66217ced693e
hbauhvagner <hbauhvagner@gmail.com>
2023-10-29 00:58:20 -0300

    Component Scan

--- src/main/java/br/com/vagnernascimento/primeiroprojetospringboot/PrimeiroProjetoSpringbootApplication.java
+++ src/main/java/br/com/vagnernascimento/primeiroprojetospringboot/PrimeiroProjetoSpringbootApplication.java
@@ -2,8 +2,10 @@ package br.com.vagnernascimento.primeiroprojetospringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.vagnernascimento")
 public class PrimeiroProjetoSpringbootApplication {
 
 	public static void main(String[] args) {
