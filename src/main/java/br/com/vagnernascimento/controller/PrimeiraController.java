1b13c897db7676d967557c08d9bfa9b448fd8b99
hbauhvagner <hbauhvagner@gmail.com>
2023-10-30 00:45:15 -0300

    Body Params

--- src/main/java/br/com/vagnernascimento/controller/PrimeiraController.java
+++ src/main/java/br/com/vagnernascimento/controller/PrimeiraController.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +27,14 @@ public class PrimeiraController {
     @GetMapping("/metodoComQueryParams2")
     public String metodoComQueryParams2(@RequestParam Map<String, String> allParams) {
         return "O parametro com metodoComQueryParams2 é " + allParams.entrySet();
+    }
+
+    @PostMapping("/metodoComBodyParams")
+    public String metodoComBodyParams(@RequestBody Usuario usuario) {
+        return "metodoComBodyParams " + usuario.username;
+    }
+
+    record Usuario(String username) {
+
     }
 }
